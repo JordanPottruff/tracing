40527f1dbc7a4ec57ccca23fb2c07148b6557cae
JordanPottruff <jordanpottruff@gmail.com>
2023-01-28 16:56:51 -0800

    Setup boilerplate.

---
+++ src/main/java/com/github/jordanpottruff/tracing/Main.java
@@ -0,0 +1,10 @@
+package tracing;
+
+import com.github.jordanpottruff.jgml.Vec3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Vec3(1.0, 2.0, 3.0));
+    }
+}
